8ca546c7e8298e023e11fa0430782797bd15feda
Lars Kusch <lars@lars-kusch.de>
2025-03-15 09:47:42 +0100

    Erste Schritte der Logikimplementierung
    Signed-off-by: Lars Kusch <lars@lars-kusch.de>

--- helpers/helpers.py
+++ helpers/helpers.py
@@ -2,15 +2,69 @@ from helpers.countries import Country
 import pycountry
 import gettext
 
-german = gettext.translation('iso3166-1', pycountry.LOCALES_DIR, languages=['de'])
+german = gettext.translation("iso3166-1", pycountry.LOCALES_DIR, languages=["de"])
 german.install()
 
 
 def get_countries() -> list[Country]:
-	"""
-	Returns a list of Country objects representing all countries in pycountry.
-	"""
-	countries = []
-	for country in pycountry.countries:
-		countries.append(Country(_(country.name), country.alpha_2))
-	return countries
+    """
+    Returns a list of Country objects representing all countries in pycountry.
+    """
+    countries = []
+    for country in pycountry.countries:
+        countries.append(Country(_(country.name), country.alpha_2))
+    return countries
+
+
+class Handelsstufe:
+    """
+    Represents a company in a chain transaction.
+    """
+
+    def __init__(self, country: Country, identifier: int = 0, max_identifier: int = 0):
+        self.country = country
+        self.is_reciever = False
+        self.is_supplier = False
+        self.responsible_for_customs = False
+        self.next_company: list[Handelsstufe] = []
+        self.previous_company: list[Handelsstufe] = []
+        self.identifier = identifier
+        self.max_identifier = max_identifier
+        if self.identifier > max_identifier:
+            raise ValueError(
+                "The identifier must be less than or equal to the max_identifier."
+            )
+        self.changed_vat = False
+        self.new_country: Country = None
+
+    def __repr__(self):
+        if self.identifier == 0:
+            return f"Verkäufer - {self.country}"
+        elif self.identifier == self.max_identifier - 1:
+            return f"Empfänger - {self.country}"
+        else:
+            return f"Handelsstufe - {self.country}"
+
+    def add_previous_company_to_chain(self, company, previous_company):
+        """
+        Adds a company to a chain transaction.
+        """
+        company.previous_company = previous_company
+        previous_company.next_company = company
+        return company
+
+    def add_next_company_to_chain(self, company, next_company):
+        """
+        Adds a company to a chain transaction.
+        """
+        company.next_company = next_company
+        next_company.previous_company = company
+        return company
+
+    def set_changed_vat_id(self, new_country: Country):
+        """
+        Sets the new country for the company.
+        """
+        self.new_country = new_country
+        self.changed_vat = True
+        return self
--- submodules/1_USt-Reihen.py
+++ submodules/1_USt-Reihen.py
@@ -1,20 +1,19 @@
 import streamlit as st
-import pycountry
-from helpers.helpers import get_countries
-from helpers.countries import Country
+from helpers.helpers import get_countries, Handelsstufe
+from graphviz import Digraph
 
 st.title("USt-Reihengeschäfte")
-
 
 # Liste der verfügbaren Länder auf Deutsch
 laender = get_countries()
 schritt = 0
 
+
 with st.expander("Grundlegende Daten"):
     st.subheader("Schritt 1: Beteiligte Firmen und Länderauswahl")
     # Anzahl der beteiligten Firmen festlegen
     anzahl_firmen = st.number_input(
-        "Anzahl der beteiligten Firmen:", min_value=1, step=1
+        "Anzahl der beteiligten Firmen:", min_value=1, step=1, value=3
     )
     if anzahl_firmen > 2:
         st.divider()
@@ -23,39 +22,58 @@ with st.expander("Grundlegende Daten"):
             st.subheader("Schritt 2: Wähle das Land für jede Firma")
             laender_firmen = []
             for i in range(int(anzahl_firmen)):
-                land = st.selectbox(f"Firma {i+1}:", laender, key=f"firma_{i}")
-                laender_firmen.append(land)
+                if i == 0:
+                    land = st.selectbox(f"Verkäufer:", laender, key=f"firma_{i}")
+                elif i == anzahl_firmen - 1:
+                    land = st.selectbox(f"Empfänger:", laender, key=f"firma_{i}")
+                else:
+                    land = st.selectbox(
+                        f"Zwischenhändler {i}:", laender, key=f"firma_{i}"
+                    )
+                laender_firmen.append(Handelsstufe(land, i, int(anzahl_firmen)))
             st.divider()
             # Ausgabe der ausgewählten Länder
             if laender_firmen:
                 st.subheader("Schritt 3: Besondere Merkmale der Firmen")
                 for i, land in enumerate(laender_firmen):
-                    land: Country
+                    land: Handelsstufe
                     container_land = st.container(border=True)
                     coulumn1, column2 = container_land.columns(
                         (
                             1,
-                            6,
+                            8,
                         )
                     )
-                    if land.flag:
-                        coulumn1.image(land.flag, width=100)
-                    column2.write(
-                        f"**{i+1}: {land.name} - {'EU' if land.EU else 'Drittland'}**"
-                    )
+                    if land.country.flag:
+                        coulumn1.image(land.country.flag, width=100)
+
+                    if i == 0:
+                        column2.write(
+                            f"**Verkäufer: {land.country.name} - {'EU' if land.country.EU else 'Drittland'}**"
+                        )
+                    elif i == int(anzahl_firmen) - 1:
+                        column2.write(
+                            f"**Empfänger: {land.country.name} - {'EU' if land.country.EU else 'Drittland'}**"
+                        )
+                    else:
+                        column2.write(
+                            f"**Zwischenhändler {i}: {land.country.name} - {'EU' if land.country.EU else 'Drittland'}**"
+                        )
                     ust = column2.checkbox(
                         "Vom Heimatland abweichende USt-ID",
                         key=f"abweichende_ust_id_{i}",
                     )
                     if ust:
                         laender_ohne_eigenes = [
-                            l for l in laender if l.code != land.code
+                            l for l in laender if l.code != land.country.code
                         ]
-                        column2.selectbox(
+                        target_country_vat_id = column2.selectbox(
                             f"Land der verwendeten USt-ID:",
                             laender_ohne_eigenes,
                             key=f"land_abweichende_ust_id_{i}",
                         )
+                        land.set_changed_vat_id(target_country_vat_id)
+
             schritt = 1
         else:
             schritt = 0
@@ -67,5 +85,27 @@ with st.expander("Grundlegende Daten"):
         st.error("Ein Handelsgeschäft benötigt mind. zwei beteiligte Firmen.")
 if schritt == 1:
     with st.expander("Bewegte und ruhende Lieferung"):
-        st.selectbox("Welche Firma transportiert die Ware?", laender_firmen)
-        st.selectbox("Welche Firma erhält die Ware?", laender_firmen)
+        transport_firma: Handelsstufe = st.selectbox(
+            "Welche Firma transportiert die Ware?", ["keine Auswahl"] + laender_firmen
+        )
+        if isinstance(transport_firma, Handelsstufe):
+            transport_firma.is_supplier = True
+        if transport_firma != "keine Auswahl":
+            erhaltende_firma_laender_moeglich = [
+                l for l in laender_firmen if l.identifier != transport_firma.identifier
+            ]
+        else:
+            erhaltende_firma_laender_moeglich = laender_firmen
+
+        erhaltende_firma = st.selectbox(
+            "Welche Firma erhält die Ware?",
+            ["keine Auswahl"] + erhaltende_firma_laender_moeglich,
+        )
+        if isinstance(erhaltende_firma, Handelsstufe):
+            erhaltende_firma.is_receiver = True
+
+        customs_import = st.selectbox(
+            "Wer übernimmt die Zollabwicklung?", ["keine Auswahl"] + laender_firmen
+        )
+        if isinstance(customs_import, Handelsstufe):
+            customs_import.responsible_for_customs = True
